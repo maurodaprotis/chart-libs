9bfd73da6bb5028fa8e4831beb4f09c743f3648a
Mauro Daprotis <maurodaprotis@gmail.com>
2023-01-30 15:52:21 -0300

    feat: add recharts page (#1)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ export const Layout = ({
   title: string;
 }) => {
   return (
-    <div className="max-w-7xl mx-auto min-h-screen mt-12 w-full px-12">
+    <div className="max-w-7xl mx-auto min-h-screen mt-12 w-full px-12 space-y-8">
       <h1 className="text-center text-2xl mb-6">{title}</h1>
       {children}
     </div>
--- src/pages/recharts.tsx
+++ src/pages/recharts.tsx
@@ -1,13 +1,118 @@
 import { Card } from "@/components/Card";
 import { Layout } from "@/components/Layout";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Cell,
+  ComposedChart,
+  LabelList,
+  Legend,
+  Line,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { donutData, barData, barLineDataGrouped } from "@/utils/data";
+
+const pieData = donutData
+  .map((d) => ({ name: d.label, value: d.value }))
+  .sort((a, b) => b.value - a.value);
+const COLORS = ["#6938EF", "#1570EF", "#D1FADF", "#DC6803", "#CA8504"];
 
 const Recharts = () => {
   return (
     <Layout title="Recharts">
-      <Card.Root>
-        <Card.Title>Recharts</Card.Title>
-        <div className="h-11 w-full bg-slate-100"></div>
-      </Card.Root>
+      <div className="flex w-full gap-8">
+        <Card.Root>
+          <Card.Title>Asset type over the years</Card.Title>
+          <div className="h-[314px] w-full">
+            <ResponsiveContainer>
+              <PieChart>
+                <Tooltip />
+                <Legend verticalAlign="bottom" height={36} />
+                <Pie
+                  data={pieData}
+                  cx="50%"
+                  cy="50%"
+                  innerRadius={45}
+                  outerRadius={100}
+                  fill="#8884d8"
+                  startAngle={90}
+                  endAngle={-270}
+                  dataKey="value"
+                  nameKey="name"
+                  minAngle={5}
+                  label
+                >
+                  {/* <LabelList
+                    dataKey="name"
+                    position="outside"
+                    offset={20}
+                    fill="#666"
+                    stroke="#666"
+                  /> */}
+                  {pieData.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                      cx="140"
+                      cy="120"
+                      // strokeWidth={4}
+                    />
+                  ))}
+                </Pie>
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
+        </Card.Root>
+        <Card.Root>
+          <Card.Title>Asset type over the years</Card.Title>
+          <div className="h-[314px] w-full">
+            <ResponsiveContainer>
+              <BarChart data={barData}>
+                <CartesianGrid />
+                <Tooltip />
+                <XAxis dataKey="label" />
+                <Legend verticalAlign="bottom" height={36} />
+                <YAxis fill="#666" />
+                <Bar data={barData} dataKey="value" fill="#FEC84B">
+                  {/* {bdata.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                  ))} */}
+                </Bar>
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </Card.Root>
+      </div>
+      <div className="flex w-full">
+        <Card.Root>
+          <Card.Title>Asset type over the years</Card.Title>
+          <div className="h-[314px] w-full">
+            <ResponsiveContainer>
+              <ComposedChart data={barLineDataGrouped}>
+                <CartesianGrid />
+                <Tooltip />
+                <XAxis dataKey="label" />
+                <Legend verticalAlign="bottom" height={36} />
+                <YAxis yAxisId="left" />
+                <YAxis yAxisId="right" orientation="right" />
+                <Bar yAxisId="left" dataKey="deals" fill="#d6bbfb"></Bar>
+                <Line
+                  yAxisId="right"
+                  type="monotone"
+                  dataKey="ltv"
+                  fill="#1570EF"
+                ></Line>
+              </ComposedChart>
+            </ResponsiveContainer>
+          </div>
+        </Card.Root>
+      </div>
     </Layout>
   );
 };
